6c0102617fcef20fb9556521f0d645f99030b00c
jiyoungnim <cloundyon31@gmail.com>
2024-11-25 11:10:35 +0900

    📝 과제 ing... (3) - 중첩라우팅으로 디테일페이지에 편집페이지를 자식페이지로서 추가

--- workspace/app/todolist/04-router/src/pages/TodoDetail.jsx
+++ workspace/app/todolist/04-router/src/pages/TodoDetail.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
 
 function TodoDetail() {
   const { _id } = useParams();
@@ -13,9 +13,12 @@ function TodoDetail() {
         <div>상태 : 미완료</div>
         <div>작성일 : 2024.11.25 12:23:45</div>
         <div>수정일 : 2024.11.25 13:45:12</div>
-        <Link to="/edit">수정</Link>
+        {/* 2️⃣ 절대경로가 아닌 상대경로로! list/(숫자) = . 이거 뒤에 /edit을 붙여라(만약 유저가 수정 버튼을 누르면) */}
+        <Link to="./edit">수정</Link>
         <Link to="/list">목록</Link>
       </div>
+
+      <Outlet />
     </div>
   );
 }
--- workspace/app/todolist/04-router/src/routes.jsx
+++ workspace/app/todolist/04-router/src/routes.jsx
@@ -10,8 +10,10 @@ import TodoList from "@pages/TodoList";
 const router = createBrowserRouter(
   [
     {
+      // 로컬호스트/ => <Layout /> 출력
       path: "/",
       element: <Layout />,
+      // <Layout /> - <Outlet />부분에 칠드런 중 하나 출력
       children: [
         // 로컬호스트(/)까지만 입력하면 아래 Navigate로 지정한 url(to)로 이동해라
         { index: true, element: <Navigate to="/home" /> },
@@ -21,9 +23,20 @@ const router = createBrowserRouter(
         { path: "add", element: <TodoAdd /> },
         { path: "edit", element: <TodoEdit /> },
         { path: "detail", element: <TodoDetail /> },
+        // 1️⃣ 동적 세그먼트
         // 위의 url은 바뀌지 않는다. 상세페이지는 유저가 무엇을 선택했는지 알려줘야 하므로 고유한 id값을 부여해야 하고 이걸 동적 세그먼트라고 한다.
-        // 바뀔 수 있는 부분을 :로 시작하는 원하는 이름으로 붙여주면 된다.
-        { path: "list/:_id", element: <TodoDetail /> },
+        // 바뀔 수 있는 부분을 :로 시작하는 원하는 이름으로 붙여주면 된다. (ex. :_id)
+        // ✨ ~/list/3 => <TodoDetail/> - 수정 버튼(⭐️to = .edit/⭐️ ...Url이 쭉 이어져야 하므로 상대경로) 누를시,
+        // <TodoDetail /> - <Outlet /> 부분에 edit 페이지 출력
+        {
+          path: "list/:_id",
+          // :으로 시작하는 _id라는 변수를 지정하여 해당 element(=TodoDetail)에서 useParams() 훅(객체 리턴)을 이용해 컨솔로 확인 가능
+          element: <TodoDetail />,
+          children: [{ path: "edit", element: <TodoEdit /> }],
+        },
+        // 2️⃣ 중첩 라우팅
+        // 각각의 아이템(:_id)의 수정 버튼 눌렀을 때, detail(상세보기)와 edit(편집창)이 함께 보이게끔 하고 싶다..
+        // 자식 페이지가 나타날 부분에 <Outlet />을 추가
       ],
     },
   ],
