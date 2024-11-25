b0c9125a1508e214161057bd980aa2ccfb5526ba
jiyoungnim <cloundyon31@gmail.com>
2024-11-25 09:39:04 +0900

    📝 과제 ing... (1)

--- workspace/app/todolist/04-router/src/routes.jsx
+++ workspace/app/todolist/04-router/src/routes.jsx
@@ -1,15 +1,25 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Layout from "@components/Layout";
 import Home from "@pages/Home";
 import About from "@pages/About";
+import TodoEdit from "@pages/TodoEdit";
+import TodoAdd from "@pages/TodoAdd";
+import TodoDetail from "@pages/TodoDetail";
+import TodoList from "@pages/TodoList";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
+      // 로컬호스트(/)까지만 입력하면 아래 Navigate로 지정한 url(to)로 이동해라
+      { index: true, element: <Navigate to="/home" /> },
       { path: "home", element: <Home /> },
       { path: "about", element: <About /> },
+      { path: "list", element: <TodoList /> },
+      { path: "add", element: <TodoAdd /> },
+      { path: "edit", element: <TodoEdit /> },
+      { path: "detail", element: <TodoDetail /> },
     ],
   },
 ]);
