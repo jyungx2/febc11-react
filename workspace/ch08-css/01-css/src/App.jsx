cb624c26b34502902965b4b55207c19bfc25cfa2
jiyoungnim <cloundyon31@gmail.com>
2024-12-02 13:14:53 +0900

    📝 규모가 작은 프로젝트 - css파일을 컴포넌트에서 직접 임포트해서 사용

--- workspace/ch08-css/01-css/src/App.jsx
+++ workspace/ch08-css/01-css/src/App.jsx
@@ -1,20 +1,27 @@
 import Button from "@components/Button";
 import "./App.css";
+import Login from "./Login";
 
 function App() {
   return (
     <>
       <h1>CSS 모듈 사용</h1>
-      <Button className="button">그냥 버튼</Button>
-      <Button bg="blue" color="red">
-        파란 배경의 빨간 글자
-      </Button>
-      <Button bg="yellow" color="red">
-        노란 배경의 빨간 글자
-      </Button>
-      <Button bg="gray" color="blue">
-        회색 배경의 파란 글자
-      </Button>
+
+      <div className="container">
+        <Button className="button">그냥 버튼</Button>
+        <Button bg="blue" color="red">
+          파란 배경의 빨간 글자
+        </Button>
+        <Button bg="yellow" color="red">
+          노란 배경의 빨간 글자
+        </Button>
+        <Button bg="gray" color="blue">
+          회색 배경의 파란 글자
+        </Button>
+      </div>
+
+      <Login />
+      {/* 아무리 App.css에 container, color-blue-red와 같은 클래스에 스타일링을 지정해도, Login.css에 의해 덮여씌여지기 때문에 적용되지 않는다. */}
     </>
   );
 }
