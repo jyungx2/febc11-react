ffae05f68007af67451b4bdfbab7e392f364a3a4
jiyoungnim <cloundyon31@gmail.com>
2024-12-12 15:48:27 +0900

    ✨ ch10-nextjs/01-manual

---
+++ workspace/ch10-nextjs/01-manual/app/layout.jsx
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head>
+        <title>Next.js App</title>
+      </head>
+
+      <body>{children}</body>
+    </html>
+  );
+}
