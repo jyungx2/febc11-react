ff92e4c774097b29f26249e84ebabb5772636b27
jiyoungnim <cloundyon31@gmail.com>
2024-12-16 23:09:24 +0900

    📝 final project - HTML/CSS 구현 시기에 게시글 등록 페이지 연습하기

---
+++ workspace/ch11-skeleton/lion-board-practice/src/pages/board/CommentList.jsx
@@ -0,0 +1,16 @@
+import CommentListItem from "@pages/board/CommentListItem";
+import CommentNew from "@pages/board/CommentNew";
+
+export default function CommentList() {
+  return (
+    <main className="container mx-auto mt-4 px-4">
+      <section className="mb-8">
+        <h4 className="mt-8 mb-4 ml-2">댓글 2개</h4>
+
+        <CommentListItem />
+
+        <CommentNew />
+      </section>
+    </main>
+  );
+}
